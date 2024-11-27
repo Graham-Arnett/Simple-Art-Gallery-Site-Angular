08916b5214ef019885a83934c094bcf2c2a0fb97
Graham Arnett <GrahamKentArnett@outlook.com>
2024-11-27 15:11:56 -0500

    menu component generated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GalleryListComponent } from './feature/gallery-list/gallery-list.component';
+import { MenuComponent } from './core/menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GalleryListComponent
+    GalleryListComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
