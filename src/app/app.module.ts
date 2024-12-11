c3eb159fdebc3f1987ab14ce4845c4ee1961fce3
Graham Arnett <GrahamKentArnett@outlook.com>
2024-12-11 15:09:54 -0500

    added homepage component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GalleryListComponent } from './feature/gallery/gallery-list/gallery-list.component';
 import { MenuComponent } from './core/menu/menu.component';
+import { HomePageComponent } from './feature/home-page/home-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GalleryListComponent,
-    MenuComponent
+    MenuComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
